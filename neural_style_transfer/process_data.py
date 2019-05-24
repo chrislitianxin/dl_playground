0c64b9e1d89d0f64d2c684f0c9bcb60add0e75cd
TX <chrislitianxin@gmail.com>
2019-05-24 15:04:05 -0400

    :tada: neural style transfer

---
+++ neural_style_transfer/process_data.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import tensorflow.contrib.eager as tfe
+
+from tensorflow.python.keras.preprocessing import image as kp_image
+from tensorflow.python.keras import models 
+from tensorflow.python.keras import losses
+from tensorflow.python.keras import layers
+from tensorflow.python.keras import backend as K
+
+def load_and_process_img(path_to_img):
+  img = load_img(path_to_img)
+  img = tf.keras.applications.vgg19.preprocess_input(img)
+  return img
+
+def deprocess_img(processed_img):
+  x = processed_img.copy()
+  if len(x.shape) == 4:
+    x = np.squeeze(x, 0)
+  assert len(x.shape) == 3, ("Input to deprocess image must be an image of "
+                             "dimension [1, height, width, channel] or [height, width, channel]")
+  if len(x.shape) != 3:
+    raise ValueError("Invalid input to deprocessing image")
+  
+  # perform the inverse of the preprocessiing step
+  x[:, :, 0] += 103.939
+  x[:, :, 1] += 116.779
+  x[:, :, 2] += 123.68
+  x = x[:, :, ::-1]
+
+  x = np.clip(x, 0, 255).astype('uint8')
+  return x
